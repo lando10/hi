d5b2f1a2693de5d20e64ede9e5164040c9a83146
Lando <jacquesalande@gmail.com>
2016-05-27 03:49:37 -0400

    Add

---
+++ hi/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset ="utf-8">
+	</head>
+	<style type="text/css">
+	body{
+		background-color: #2ecc71;
+		color:white;
+	}
+	h1{
+		position:absolute;
+		top:40%;
+		width:100%;
+		font-family:Helvetica;
+		font-size:100;
+		text-align:center;
+	}
+	</style>
+	<body>
+	
+		<h1> Je t'aime ma sublime creature Essu</h1>
+
+	</body>
+</html>
